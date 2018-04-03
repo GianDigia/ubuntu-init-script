ff83c5b1347295285cadf67a0ce6e9ce07c2162b
Gianmarco <digiacomogianmarco@gmail.com>
2018-04-03 15:01:36 +0200

    remove 'delete firefox'

--- script.sh
+++ script.sh
@@ -3,15 +3,6 @@ sudo apt -y remove --purge libreoffice*
 sudo apt -y clean
 sudo apt -y autoremove
 
-#Remove firefox
-sudo apt -y purge firefox
-sudo rm -d -f -r ~/.mozilla/firefox/
-sudo rm -d -f -r .macromedia/
-sudo rm -d -f -r .adobe
-sudo rm -d -f -r /etc/firefox/
-sudo rm -d -f -r /usr/lib/firefox/
-sudo rm -d -f -r /usr/lib/firefox-addons/
-
 #Prepare Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
